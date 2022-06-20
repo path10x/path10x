643e6844b1bf4f8c4a38a27e87402aa5e6977683
Max Cook <maxwell.h.cook@gmail.com>
2022-06-20 16:59:27 -0700

    Boilerplate code. Co-authored-by: Sam Carter <65371360+cartersamj@users.noreply.github.com> Co-authored-by: Young Kim <101305992+youngs-kim@users.noreply.github.com> Co-authored-by: Anjanie McCoy <100004879+anjaniemccoy@users.noreply.github.com> Co-authored-by: Anjanie McCoy <100004879+anjaniemccoy@users.noreply.github.com>

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
+
 app.use(express.json())
 app.use(express.urlencoded())
 app.get('/', ((req: Request, res: Response) => {
