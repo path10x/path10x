9414931e713b775dd2e1067dbdd089337d78babd
Youngs-kim <doppio0518@gmail.com>
2022-06-21 12:28:29 -0700

    set up get all algos by user id.
    Co-authored-by: Sam Carter <65371360+cartersamj@users.noreply.github.com>

--- server/controller/algorithmController.ts
+++ server/controller/algorithmController.ts
@@ -1,3 +1,29 @@
 import { Request, Response, NextFunction } from 'express';
+const db = require('../models/AlgorithmModel.ts');
 
-export const algorithmController = {};
+interface ExpressMiddleware {
+  req: Request;
+  res: Response;
+  next: NextFunction;
+}
+
+export const algorithmController = {
+  getUserAlgorithms: async ({ req, res, next }: ExpressMiddleware) => {
+    try {
+      const userID = req.params.user;
+      const SQLQuery = `
+      GET * FROM "public.UserAlgorithms"
+      WHERE user_id=$1
+      `;
+      const response = await db.query(SQLQuery, [userID]);
+      res.locals = response.rows;
+      return next();
+    } catch (err) {
+      return next({
+        log: 'error occurred in algorithmController.getUserAlgorithms',
+        status: 400,
+        message: { err },
+      });
+    }
+  },
+};
